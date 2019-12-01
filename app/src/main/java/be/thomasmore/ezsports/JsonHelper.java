334772daef95d1f953af9447668313fd6ad87012
Bosz <r0707151@student.thomasmore.be>
2019-12-01 16:03:15 +0100

    Extra activities, JsonHelper, HttpWriter en HttpReader toegevoegd.

---
+++ app/src/main/java/be/thomasmore/ezsports/JsonHelper.java
@@ -0,0 +1,13 @@
+package be.thomasmore.ezsports;
+
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JsonHelper {
+}
