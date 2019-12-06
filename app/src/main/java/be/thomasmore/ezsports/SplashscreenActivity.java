0e904b7ffb3a0a9fb145eca90e43a43c6e6cd5f0
Bosz <r0707151@student.thomasmore.be>
2019-12-06 14:02:31 +0100

    Splashscreen toegevoegd

--- app/src/main/java/be/thomasmore/ezsports/SplashscreenActivity.java
+++ app/src/main/java/be/thomasmore/ezsports/SplashscreenActivity.java
@@ -1,32 +1,28 @@
 package be.thomasmore.ezsports;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
+import android.os.Handler;
 
 public class SplashscreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splashscreen);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void run() {
+                // This method will be executed once the timer is over
+                Intent i = new Intent(SplashscreenActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
             }
-        });
+        }, 5000);
+
     }
 
 }
