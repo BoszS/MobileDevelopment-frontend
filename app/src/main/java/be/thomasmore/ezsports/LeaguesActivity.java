53d3fcedebc068ce7f5f5a09927ca3559c462542
Bosz <r0707151@student.thomasmore.be>
2019-12-07 15:12:47 +0100

    LeaguesActivity

---
+++ app/src/main/java/be/thomasmore/ezsports/LeaguesActivity.java
@@ -0,0 +1,25 @@
+package be.thomasmore.ezsports;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class LeaguesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leagues);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+}
