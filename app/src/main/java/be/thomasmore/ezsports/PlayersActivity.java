4b2ca3093ce9563665268a33fc66c900368a31a9
Bosz <r0707151@student.thomasmore.be>
2019-12-18 23:17:10 +0100

    zoekfunctie voor players

--- app/src/main/java/be/thomasmore/ezsports/PlayersActivity.java
+++ app/src/main/java/be/thomasmore/ezsports/PlayersActivity.java
@@ -10,7 +10,9 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -35,6 +37,8 @@ public class PlayersActivity extends AppCompatActivity {
         setGame(game);
 
         leesPlayers();
+
+
     }
 
     private void setGame(String game){
@@ -52,11 +56,44 @@ public class PlayersActivity extends AppCompatActivity {
                 PlayersAdapter playersAdapter = new PlayersAdapter(getApplicationContext(), players);
 
                 final ListView listViewPlayers = (ListView)findViewById(R.id.listViewPlayers);
+                listViewPlayers.setAdapter(playersAdapter);
+
+            }
+        });
+        httpReader.execute("https://api.pandascore.co/" + game + "/players?token=" + apiKey);
+
+
+    }
+
+    private void searchPlayer(String searchString) {
+        HttpReader httpReader = new HttpReader();
+        httpReader.setOnResultReadyListener(new HttpReader.OnResultReadyListener() {
+            @Override
+            public void resultReady(String result) {
+                JsonHelper jsonHelper = new JsonHelper();
+                final List<Player> players = jsonHelper.getPlayers(result);
+
+                PlayersAdapter playersAdapter = new PlayersAdapter(getApplicationContext(), players);
+
+                final ListView listViewPlayers = (ListView)findViewById(R.id.listViewPlayers);
 
                 listViewPlayers.setAdapter(playersAdapter);
             }
         });
-        httpReader.execute("https://api.pandascore.co/" + game + "/players?token=" + apiKey);
+        httpReader.execute("https://api.pandascore.co/" + game + "/players?search[name]=" + searchString  + "&token=" + apiKey);
     }
 
+    public void searchPlayer_click(View v) {
+        EditText playerName = (EditText)findViewById(R.id.searchPlayer);
+
+        if (playerName.getText().toString() == "") {
+            leesPlayers();
+        } else {
+            searchPlayer(playerName.getText().toString());
+        }
+    }
+
+    private void toon(String tekst) {
+        Toast.makeText(getBaseContext(), tekst, Toast.LENGTH_SHORT).show();
+    }
 }
