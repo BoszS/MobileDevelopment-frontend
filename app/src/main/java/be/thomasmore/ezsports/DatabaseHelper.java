a93b9969aa7987f3daca81395313358a3cb2fa58
Bosz <r0707151@student.thomasmore.be>
2019-12-19 11:36:18 +0100

    DatabaseHelper configureren

---
+++ app/src/main/java/be/thomasmore/ezsports/DatabaseHelper.java
@@ -0,0 +1,71 @@
+package be.thomasmore.ezsports;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    // Database Version
+    private static final int DATABASE_VERSION = 1;
+    // Database Name
+    private static final String DATABASE_NAME = "ezsports";
+
+    // uitgevoerd bij instantiatie van DatabaseHelper
+    // -> als database nog niet bestaat, dan creëren (call onCreate)
+    // -> als de DATABASE_VERSION hoger is dan de huidige versie,
+    //    dan upgraden (call onUpgrade)
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    // methode wordt uitgevoerd als de database gecreëerd wordt
+    // hierin de tables creëren en opvullen met gegevens
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_TABLE_PRONOSTIC = "CREATE TABLE pronostic (" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "name TEXT," +
+                "matchId INTEGER," +
+                "team1Id INTEGER," +
+                "team2Id INTEGER," +
+                "team1Name TEXT," +
+                "team2Name TEXT, " +
+                "prognosticTeam1 INTEGER," +
+                "prognosticTeam2 INTEGER)";
+        db.execSQL(CREATE_TABLE_PRONOSTIC);
+
+        String CREATE_TABLE_PLAYER = "CREATE TABLE player (" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "name TEXT," +
+                "first_name TEXT, " +
+                "last_name TEXT, " +
+                "hometown TEXT)";
+
+        db.execSQL(CREATE_TABLE_PLAYER);
+    }
+
+    // methode wordt uitgevoerd als database geupgrade wordt
+    // hierin de vorige tabellen wegdoen en opnieuw creëren
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS prognostic");
+        db.execSQL("DROP TABLE IF EXISTS player");
+
+        // Create tables again
+        onCreate(db);
+    }
+
+    //-------------------------------------------------------------------------------------------------
+    //  CRUD Operations
+    //-------------------------------------------------------------------------------------------------
+    public long insertPrognostic() {
+
+    }
+}
